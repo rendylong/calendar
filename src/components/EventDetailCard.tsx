401470497b92b431b9c9e8adb143361290fed82a
rendylong <didobang@foxmail.com>
2025-01-10 17:56:32 +0800

    v1.4.1

--- src/components/EventDetailCard.tsx
+++ src/components/EventDetailCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import {
   CalendarDays,
